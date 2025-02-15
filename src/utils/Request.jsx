74e447cbac707a37f69cf8768b63b986756ce1e9
Surafel-Adamu <suratechx@gmail.com>
2025-02-15 19:26:37 +0300

    axios and request

---
+++ src/utils/Request.jsx
@@ -0,0 +1,13 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`, // 213 is the id for Netflix
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`, // 28 is the id for Action
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`, // 35 is the id for Comedy
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`, // 27 is the id for Horror
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`, // 10749 is the id for Romance
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`, // 99 is the id for Documentaries
+    fetchTvShows: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
+}
+export default requests;
