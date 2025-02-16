6e3cb85d71be71ee95535e0170911f400f4c627c
Surafel-Adamu <suratechx@gmail.com>
2025-02-16 13:25:03 +0300

    Banner

--- src/utils/Request.jsx
+++ src/utils/Request.jsx
@@ -1,4 +1,5 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = import.meta.env.VITE_API_KEY;;
+
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`, // 213 is the id for Netflix
@@ -8,6 +9,6 @@ const requests = {
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`, // 27 is the id for Horror
     fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`, // 10749 is the id for Romance
     fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`, // 99 is the id for Documentaries
-    fetchTvShows: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
+    fetchTvShows: `/tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
 }
 export default requests;
